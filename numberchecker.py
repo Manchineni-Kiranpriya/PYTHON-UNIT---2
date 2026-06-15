1021dfb6c18d4927cf9fcf88ccf7832c25bd9c88
Manchineni Kiran Priya <100516424+Manchineni-Kiranpriya@users.noreply.github.com>
2026-06-15 13:24:32 +0530

    Add files via upload

---
+++ numberchecker.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+if number == 0:
+    print(number, "is Zero")
+elif number > 0:
+    print(number, "is a Positive number")
+else:
+    print(number, "is a Negative number")
+             
